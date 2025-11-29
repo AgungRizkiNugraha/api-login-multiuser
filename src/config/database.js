c4202c18f0fa7632a0461985354e26b32f94b6a6
AgungRizkiNugraha <agungrizki640@gmail.com>
2025-11-29 11:55:58 +0700

    edit file vercel

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,13 @@
-const mysql = require('mysql2');
-const db = mysql.createConnection({
-host: process.env.DB_HOST,
-user: process.env.DB_USER,
-password: process.env.DB_PASSWORD,
-database: process.env.DB_NAME
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
-db.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the MySQL database.');
+pool.connect((err) => {
+  if (err) console.error("Error connecting to PostgreSQL:", err);
+  else console.log("Connected to PostgreSQL.");
 });
-module.exports = db;
+
+module.exports = pool;
