fff63f21bdfc12d7b78ddcd2e3ce6a2f4ba59adf
AgungRizkiNugraha <agungrizki640@gmail.com>
2025-11-29 10:47:52 +0700

    edit file vercel

--- src/index.js
+++ src/index.js
@@ -9,6 +9,5 @@ app.use(express.json());
 // Routes
 app.use(cors());
 app.use("/api/auth", require("../src/routes/authRoutes"));
-app.listen(process.env.PORT, () => {
-    console.log("Server berjalan di port " + process.env.PORT);
-});
+// Export untuk Vercel, bukan listen
+module.exports = app;
